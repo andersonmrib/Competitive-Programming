faa09599f33febd64ae49163fee44c8710894a07
andersonmrib <153329538+andersonmrib@users.noreply.github.com>
2025-08-08 10:04:52 -0300

    00422 - Word-Search Wonder.cpp

--- UVA/00422 - Word-Search Wonder.cpp
+++ UVA/00422 - Word-Search Wonder.cpp
@@ -5,48 +5,46 @@ using namespace std;
 int dr[] = {1, 1, 0, -1, -1, -1, 0, 1};
 int dc[] = {0, 1, 1, 1, 0, -1, -1, -1};
 
+bool outside(int x, int y, int l){
+  return (x < 0 || x >= l || y < 0 || y >= l);
+}
+
 int main(){
 
   speedBoost;
-
   int l; cin >> l;
-  vector<string> grid;
+  vector<string> grid(l);
+  for(int i=0; i<l; i++)
+    cin >> grid[i];
 
-  for(int i=0; i<l; i++){
-    string line; cin >> line;
-    grid.push_back(line);
-  }
-
+  string word;
   while(true){
-    string word; cin >> word;
-
+    cin >> word;
     if(word == "0") break;
 
     bool found = false;
-
     for(int i=0; i<l && !found; i++){
       for(int j=0; j<l && !found; j++){
         for(int d=0; d<8; d++){
           int x = i, y = j, idx = 0;
 
           while(idx < word.size()){
-            if(x < 0 || x >= l || y < 0 || y >= l || grid[x][y] != word[idx])
-              break;
+            if(outside(x, y, l) || grid[x][y] != word[idx]) break;
             idx++;
             x += dr[d], y += dc[d];
           }
 
           if(idx == word.size()){
+            x -= dr[d], y -= dc[d];
+            cout << i+1 << "," << j+1 << " " << x+1 << "," << y+1 << "\n";
             found = true;
-            cout << i+1 << "," << j+1 << " " << x-dr[d]+1 << "," << y-dc[d]+1 << "\n";
             break;
           }
         }
       }
     }
 
-    if(!found)
-      cout << "Not found\n";
+    if(!found) cout << "Not found\n";
   }
 
   return 0;
