00e68cc8b88177d211e7b826b30bc17872ec3ac9
andersonmrib <153329538+andersonmrib@users.noreply.github.com>
2025-08-08 10:37:17 -0300

    Add files via upload

---
+++ Kattis/Take Two Stones.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define speedBoost ios::sync_with_stdio(0); cin.tie(0);
+#define int long long
+using namespace std;
+
+int32_t main(){
+
+  speedBoost;
+  int n; cin >> n;
+  if(n&1) cout << "Alice\n";
+  else    cout << "Bob\n";
+
+  return 0;
+}
