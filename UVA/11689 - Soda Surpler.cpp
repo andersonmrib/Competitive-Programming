868c910c7383dc35787d3a789fa37575e3c32950
andersonmrib <153329538+andersonmrib@users.noreply.github.com>
2025-10-17 13:45:16 -0300

    Add files via upload

---
+++ UVA/11689 - Soda Surpler.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define speedBoost ios::sync_with_stdio(0); cin.tie(0);
+#define endl "\n"
+using namespace std;
+
+int main(){
+
+  speedBoost;
+  int n; cin >> n;
+  while(n--){
+
+    int e, f, c; cin >> e >> f >> c;
+    int total_empty = e + f;
+    int sodas = 0;
+
+    // While possible to exchange
+    while(total_empty >= c){
+
+      int new_sodas = total_empty / c;
+      sodas += new_sodas;
+      int remaining = total_empty % c;
+
+      int bottles_from_new_soda = new_sodas;
+      total_empty = bottles_from_new_soda + remaining;
+    }
+
+    cout << sodas << endl;
+  }
+
+  return 0;
+}
