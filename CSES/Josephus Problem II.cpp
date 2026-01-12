d7b319f0430d86263f969f1c9da2470ceb8d2bd3
andersonmrib <153329538+andersonmrib@users.noreply.github.com>
2026-01-12 00:04:52 -0300

    Add files via upload

---
+++ CSES/Josephus Problem II.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <bits/extc++.h>
+#define speedBoost ios::sync_with_stdio(0); cin.tie(0);
+#define endl "\n"
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+             tree_order_statistics_node_update> ost;
+
+int main(){
+
+  speedBoost;
+  int n, k; cin >> n >> k;
+  ost s;
+  for(int i=1; i<=n; i++)
+    s.insert(i);
+
+   int pos = 0;
+   while(!s.empty()){
+
+     int sz = s.size();
+     pos = (pos + k) % sz;
+
+     auto it = s.find_by_order(pos);
+     cout << *it << (sz == 1 ? "" : " ");
+     s.erase(it);
+   }
+
+  return 0;
+}
