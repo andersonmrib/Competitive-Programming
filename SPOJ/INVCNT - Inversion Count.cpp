a31046dd2bc8893a8f3111b5981f7bdca5fad5d0
andersonmrib <153329538+andersonmrib@users.noreply.github.com>
2026-02-07 22:18:57 -0300

    Add files via upload

---
+++ SPOJ/INVCNT - Inversion Count.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#define speedBoost ios::sync_with_stdio(0); cin.tie(0);
+#define int long long
+#define endl "\n"
+
+using namespace std;
+
+typedef vector<int> vi;
+
+vi a;
+int inv;
+vi merge(vi &l, vi &r){
+  int n = l.size(), m = r.size();
+  vi ans;
+  int i = 0, j = 0;
+  while(i < n && j < m){
+    if(l[i] <= r[j]) ans.push_back(l[i++]);
+    else{
+      ans.push_back(r[j++]);
+      inv += (n - i);
+    }
+  }
+
+  while(i < n) ans.push_back(l[i++]);
+  while(j < m) ans.push_back(r[j++]);
+  return ans;
+}
+
+vi merge_sort(int l, int r){
+  if(l == r) return { a[l] };
+  int mid = (l + r) >> 1;
+  vi left = merge_sort(l, mid);
+  vi right = merge_sort(mid+1, r);
+  return merge(left, right);
+}
+
+int32_t main(){
+
+  speedBoost;
+  int t; cin >> t;
+  while(t--){
+
+    int n; cin >> n;
+    a.resize(n);
+    for(int i=0; i<n; i++)
+      cin >> a[i];
+
+    inv = 0;
+    merge_sort(0, n-1);
+    cout << inv << endl;
+  }
+
+  return 0;
+}
